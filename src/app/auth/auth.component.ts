f4ffa57b1e9631873196abcf8a26f9f5740bbda9
Rayzik <mralexrabota@gmail.com>
2020-05-19 23:03:38 +0800

    create auth form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.less']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+
+  public form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.email],
+      password: ['', Validators.required]
+    });
+  }
+
+  public onSubmit() {
+    console.log(this.form.valid);
+  }
+
 }
